c4f90dbe17e8d05602bc7848f6e0b2652943cc60
Juan Camilo Quevedo Jaimes <juanqueja64@gmail.com>
2026-03-24 12:13:50 -0500

    Funcionalidad OK, ahora a estilizar

--- 01_Practica/app.js
+++ 01_Practica/app.js
@@ -12,11 +12,16 @@ btnAumentar.addEventListener("click", function () {
     cont++;
     contadorElement.textContent = cont;
     console.log(`Contador: ${cont}`);
+    if (cont >= 10) {
+        contadorElement.style.color = "red"
+    }
 });
 
 btnDecrementar.addEventListener("click", function () {
     cont--;
     contadorElement.textContent = cont;
     console.log(`Contador: ${cont}`);
-
+    if (cont <= 9) {
+        contadorElement.style.color = "green"
+    }
 });
